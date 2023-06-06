9712078966393b70609db4eb60a5bd65da0fd85b
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-06-06 14:49:37 +0300

    Solution for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let celsius = (degreesF - 32) * 5 / 9
+  return Number(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let fahrenheit = (degreesC * 9 / 5) + 32
+  return Number(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
