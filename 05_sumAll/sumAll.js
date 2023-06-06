fb7f34516551a6548cdde74db3887d0e51fd5fa0
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-06-06 14:07:53 +0300

    Solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  let answer = 0;
+  if (a >= 0 && b >= 0 && typeof a === "number" && typeof b === "number") {
+  for (let i = 0; i <= b || i <= a; i++) {
+    answer += i;
+  }
+  return answer;
+} else {
+  return "ERROR";
+}
 };
 
 // Do not edit below this line
