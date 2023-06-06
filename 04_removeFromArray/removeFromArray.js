2c6fbd5eb242c5fcbbdc51b3dbbeeb6f0904a9be
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-06-06 13:35:44 +0300

    Create different solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function(arr, ...values) {    
-  return arr.filter(ele => !values.includes(ele))
-}
+//const removeFromArray = function(arr, ...values) {    
+//  return arr.filter(ele => !values.includes(ele))
+//}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
+
+
+const removeFromArray = function(arr, ...values) {    
+  let newArray = []
+  arr.forEach(element => {
+    if (!values.includes(element)) {
+      newArray.push(element)
+    }
+  })
+    return newArray
+}
+
+module.exports = removeFromArray
