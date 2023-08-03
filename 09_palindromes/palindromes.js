5dd8c978c7907f092796b161e2d0b29c2e72c1f6
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-08-03 12:02:35 +0300

    Solutions for tasks 8 and 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let originalString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+  let reverseString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split("").reverse().join("").toLowerCase();
+  let originalNoSpaces = originalString.replace(/\s/g, "");
+  let reverseNoSpaces = reverseString.replace(/\s/g, "");
+  if (originalNoSpaces == reverseNoSpaces) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
