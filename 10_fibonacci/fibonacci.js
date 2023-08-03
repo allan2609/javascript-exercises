39c7e7cf5e16bdf8aa39b651c7e86b00aeba9825
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-08-03 13:03:19 +0300

    Solution for task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let firstInt = 0;
+  let secondInt = 1;
+  let j = Number(n);
+  if (j > 0) {
+    for (let i = 2; i < j; i++) {
+      let result = firstInt + secondInt;
+      firstInt = secondInt;
+      secondInt = result;
+    }
+    return firstInt + secondInt;
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
