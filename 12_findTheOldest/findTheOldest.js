4c34d7ba9ec2bf27f7811111a2caef0d796fa2e6
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-08-06 17:41:03 +0300

    Solution for task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const peopleMapped = arr.map(person => ({
+    name: person.name,
+    age: (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth
+}));
+  const sorted = peopleMapped.sort((a, b) => b.age - a.age);
+  return sorted[0];
 };
 
 // Do not edit below this line
